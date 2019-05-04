372b75527019bead444d9f4f349ae71d48f6297e
Kylie Anglin <kal3nh@virginia.edu>
2019-05-04 18:28:11 +0000

    Edited start-hugo.sh

--- .gitpod/start-hugo.sh
+++ .gitpod/start-hugo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 cd /workspace/website
 git submodule update --init --recursive
-cd /workspace/website/config/_default
 /workspace/hugo server -b https://1313-${GITPOD_WORKSPACE_URL#https://} --appendPort=false $@
 cat <<EOF
 "$ ./.gitpod/start-hugo.sh" to re-start hugo
